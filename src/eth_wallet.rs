ad5a4524ca9a69153a04f6373c06aec3f2854eb9
Sahil Rana <xsranhail@gmail.com>
2025-01-21 17:01:58 +0000

    Initialize Rust crypto wallet project with keypair generation and wallet functionality

---
+++ src/eth_wallet.rs
@@ -0,0 +1,66 @@
+use anyhow::{bail, Result};
+use secp256k1::{
+    rand::{rngs, SeedableRng},
+    PublicKey, SecretKey,
+};
+use serde::{Deserialize, Serialize};
+use std::io::BufWriter;
+use std::str::FromStr;
+use std::{fs::OpenOptions, io::BufReader};
+use tiny_keccak::keccak256;
+use web3::types::Address;
+
+pub fn generate_keypair()->(SecretKey,PublicKey){
+    let secp = secp256k1::Secp256k1::new();
+    let mut rng = rngs::StdRng::seed_from_u64(111);
+    secp.generate_keypair(&mut rng)
+}
+// Getting the public crypto wallet address//
+pub fn public_key_address(public_key: &PublicKey)->Address{
+    let public_key = public_key.serialize_uncompressed();
+    debug_assert_eq!(public_key[0],0x04) ;
+    let hash = keccak256(&public_key[1..]);
+    Address::from_slice(&hash[12..])
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Wallet {
+    pub secret_key: String,
+    pub public_key: String,
+    pub public_address: String,
+
+}
+
+impl Wallet {
+    pub fn new(secret_key: &SecretKey, public_key: &PublicKey) -> Self {
+        let addr: Address = public_key_address(&public_key);
+        Wallet {
+            secret_key: secret_key.to_string(),
+            public_key: public_key.to_string(),
+            public_address: format!("{:?}", addr),
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+pub fn save_to_file(&self, file_path: &str) -> Result<()> {
+    let file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open(file_path)?;
+    let buf_writer = BufWriter::new(file);
+
+    serde_json::to_writer_pretty(buf_writer, self)?;
+
+    Ok(())
+}
