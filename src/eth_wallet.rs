892f910e380e5d5f65c130305f7158dc4797350a
Sahil Rana <xsranhail@gmail.com>
2025-01-26 16:34:35 +0000

    rust wallet in icp

--- src/eth_wallet.rs
+++ src/eth_wallet.rs
@@ -1,4 +1,5 @@
-use anyhow::{bail, Result};
+use crate::utils;
+use anyhow::Result;
 use secp256k1::{
     rand::{rngs, SeedableRng},
     PublicKey, SecretKey,
@@ -10,9 +11,9 @@ use std::{fs::OpenOptions, io::BufReader};
 use tiny_keccak::keccak256;
 use web3::types::Address;
 
-pub fn generate_keypair()->(SecretKey,PublicKey){
+pub fn generate_keypair() -> (SecretKey, PublicKey) {
     let secp = secp256k1::Secp256k1::new();
-    let mut rng = rngs::StdRng::seed_from_u64(111);
+    let mut rng = rngs::JitterRng::new_with_timer(utils::get_nstime);
     secp.generate_keypair(&mut rng)
 }
 // Getting the public crypto wallet address//
@@ -28,7 +29,6 @@ pub struct Wallet {
     pub secret_key: String,
     pub public_key: String,
     pub public_address: String,
-
 }
 
 impl Wallet {
@@ -40,21 +40,17 @@ impl Wallet {
             public_address: format!("{:?}", addr),
         }
     }
-}
 
+    pub fn from_file(file_path: &str) -> Result<Wallet> {
+        let file = OpenOptions::new().read(true).open(file_path)?;
+        let buf_reader = BufReader::new(file);
 
-
-
-
-
-
-
-
-
-
-
-pub fn save_to_file(&self, file_path: &str) -> Result<()> {
-    let file = OpenOptions::new()
+        let wallet: Wallet = serde_json::from_reader(buf_reader)?;
+        Ok(wallet)
+    }
+    
+    pub fn save_to_file(&self, file_path: &str) -> Result<()> {
+        let file = OpenOptions::new()
         .write(true)
         .create(true)
         .open(file_path)?;
@@ -63,4 +59,15 @@ pub fn save_to_file(&self, file_path: &str) -> Result<()> {
     serde_json::to_writer_pretty(buf_writer, self)?;
 
     Ok(())
+}
+
+pub fn get_secret_key(&self) -> Result<SecretKey> {
+    let secret_key = SecretKey::from_str(&self.secret_key)?;
+    Ok(secret_key)
+}
+
+pub fn get_public_key(&self) -> Result<PublicKey> {
+    let pub_key = PublicKey::from_str(&self.public_key)?;
+    Ok(pub_key)
+}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 mod eth_wallet;
+mod utils;
 fn main() -> Result<()> {
     let (secret_key, pub_key) = eth_wallet::generate_keypair();
 
@@ -12,7 +13,11 @@ fn main() -> Result<()> {
     let crypto_wallet = eth_wallet::Wallet::new(&secret_key, &pub_key);
     println!("crypto_wallet: {:?}", &crypto_wallet);
 
-    crypto_wallet.save_to_file("crypto_wallet.json")?;
+    let wallet_file_path = "crypto_wallet.json";
+    crypto_wallet.save_to_file(wallet_file_path)?;
+
+    let loaded_wallet = eth_wallet::Wallet::from_file(wallet_file_path)?;
+    println!("loaded_wallet: {:?}", loaded_wallet);
 
     Ok(())
 }
